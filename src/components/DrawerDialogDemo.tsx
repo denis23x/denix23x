4daeaed515ed24921f284841cc8330ac3a1380ec
denis23x <Crazy_Code#23>
2024-11-03 19:40:13 +0700

    feat: playing with shadcn/ui

---
+++ src/components/DrawerDialogDemo.tsx
@@ -0,0 +1,85 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/components/ui/drawer"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {useIsMobile} from "@/hooks/use-mobile";
+import {ReactNode} from "react";
+
+interface ModalProps {
+	isOpen?: boolean;
+	onClose?: () => void;
+	children: ReactNode;
+}
+
+export function DrawerDialogDemo({children}: ModalProps) {
+	const [open, setOpen] = React.useState(false)
+	const isMobile = useIsMobile()
+
+	if (!isMobile) {
+		return (
+			<Dialog open={open} onOpenChange={setOpen}>
+				<DialogTrigger asChild>
+					<Button variant="outline">Edit Profile</Button>
+				</DialogTrigger>
+				<DialogContent className="sm:max-w-[425px]">
+					<DialogHeader>
+						<DialogTitle>Edit profile</DialogTitle>
+						<DialogDescription>
+							Make changes to your profile here. Click save when you're done.
+						</DialogDescription>
+					</DialogHeader>
+					<div>
+						{children}
+					</div>
+				</DialogContent>
+			</Dialog>
+		)
+	} else {
+
+
+	return (
+		<Drawer open={open} onOpenChange={setOpen}>
+			<DrawerTrigger asChild>
+				<Button variant="outline">Edit Profile</Button>
+			</DrawerTrigger>
+			<DrawerContent>
+				<DrawerHeader className="text-left">
+					<DrawerTitle>Edit profile</DrawerTitle>
+					<DrawerDescription>
+						Make changes to your profile here. Click save when you're done.
+					</DrawerDescription>
+				</DrawerHeader>
+				<div>
+					{children}
+				</div>
+				<DrawerFooter className="pt-2">
+					<DrawerClose asChild>
+						<Button variant="outline">Cancel</Button>
+					</DrawerClose>
+				</DrawerFooter>
+			</DrawerContent>
+		</Drawer>
+	)
+	}
+
+}
