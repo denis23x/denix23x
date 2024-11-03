e67c5f781ab69eb5544bcd0b8a22ad18242817da
denis23x <Crazy_Code#23>
2024-11-04 00:43:03 +0700

    feat: added colorful

---
+++ src/components/app-drawer-dialog.tsx
@@ -0,0 +1,62 @@
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+	Drawer,
+	DrawerContent,
+	DrawerDescription,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/components/ui/drawer";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { ReactNode, useState } from "react";
+
+interface AppDrawerDialogProps {
+	title: string;
+	description: string;
+	trigger: ReactNode;
+	children: ReactNode;
+}
+
+export function AppDrawerDialog({ title, description, trigger, children }: AppDrawerDialogProps) {
+	const [isOpen, setIsOpen] = useState(false);
+	const isMobile: boolean = useIsMobile();
+
+	if (isMobile) {
+		return (
+			<Drawer open={isOpen} onOpenChange={setIsOpen}>
+				<DrawerTrigger asChild>
+					<div>{trigger}</div>
+				</DrawerTrigger>
+				<DrawerContent>
+					<DrawerHeader>
+						<DrawerTitle>{title}</DrawerTitle>
+						<DrawerDescription>{description}</DrawerDescription>
+					</DrawerHeader>
+					<div className={"p-4"}>{children}</div>
+				</DrawerContent>
+			</Drawer>
+		);
+	}
+
+	return (
+		<Dialog open={isOpen} onOpenChange={setIsOpen}>
+			<DialogTrigger asChild>
+				<div>{trigger}</div>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>{title}</DialogTitle>
+					<DialogDescription>{description}</DialogDescription>
+				</DialogHeader>
+				<div>{children}</div>
+			</DialogContent>
+		</Dialog>
+	);
+}
