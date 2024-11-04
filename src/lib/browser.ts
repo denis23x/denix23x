507ad5582359ef2e4705b9498c063c8d01284e64
denis23x <Crazy_Code#23>
2024-11-04 19:02:34 +0700

    feat: added placeholder-image

--- src/lib/browser.ts
+++ src/lib/browser.ts
@@ -3,6 +3,23 @@ import { toast } from "sonner";
 export function handleCopy(text: string): void {
 	navigator.clipboard
 		.writeText(text)
-		.then(() => toast.success("Text copied to clipboard!"))
-		.catch(() => toast.error("Failed to copy text"));
+		.then(() => toast.success("Copied to clipboard!"))
+		.catch(() => toast.error("Failed to copy"));
+}
+
+export function handleShare(data: ShareData): void {
+	navigator.share(data).catch(() => toast.error("Failed to share"));
+}
+
+export function handleDownload(url: string, filename: string): void {
+	const HTMLElement: HTMLAnchorElement = document.createElement("a");
+
+	HTMLElement.download = filename;
+	HTMLElement.href = url;
+
+	document.body.appendChild(HTMLElement);
+
+	HTMLElement.click();
+
+	document.body.removeChild(HTMLElement);
 }
