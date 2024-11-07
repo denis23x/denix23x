3299b816dc3fd66d08a471f2ae0e9c4171b6f17e
denis23x <Crazy_Code#23>
2024-11-08 01:31:58 +0700

    fix: fixed build

--- src/components/app-grid-effect.tsx
+++ src/components/app-grid-effect.tsx
@@ -15,7 +15,7 @@ interface AppGridEffectProps {
 }
 
 export const AppGridEffect = ({ items, className }: AppGridEffectProps) => {
-	let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+	const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
 	return (
 		<ul className={cn("grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5", className)}>
