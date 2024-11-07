63b5af10faec793f2e93b11e06fbfdb9c8da5867
denis23x <Crazy_Code#23>
2024-11-07 13:51:01 +0700

    chore: update placeholder-api

--- src/app/dashboard/tools/placeholder-api/components/placeholder-request.tsx
+++ src/app/dashboard/tools/placeholder-api/components/placeholder-request.tsx
@@ -13,28 +13,37 @@ interface PlaceholderRequestProps {
 	path: string;
 	response: string;
 	model: string;
+	parameters: PlaceholderRequestParameters[];
 }
 
-export default function PlaceholderRequest({ type, path, response, model }: PlaceholderRequestProps) {
+export interface PlaceholderRequestParameters {
+	name: string;
+	description: string;
+	key: string;
+	type: string;
+	partOf: string;
+	required?: boolean;
+}
+
+export default function PlaceholderRequest({ type, path, response, model, parameters }: PlaceholderRequestProps) {
 	const map: Record<string, Record<string, string>> = {
 		GET: {
-			bg: "bg-[#61affe] dark:bg-[#61affe]/50",
+			bg: "bg-[rgba(97,175,254,.1)]",
+			button: "bg-[#61affe] dark:bg-[#61affe]/50",
 			border: "border border-[#61affe] dark:border-[#61affe]/50",
 		},
 		POST: {
-			bg: "bg-[#49cc90] dark:bg-[#49cc90]/50",
+			bg: "bg-[rgba(73,204,144,.1)]",
+			button: "bg-[#49cc90] dark:bg-[#49cc90]/50",
 			border: "border border-[#49cc90] dark:border-[#49cc90]/50",
 		},
 	};
 
 	return (
-		<Collapsible
-			className={`w-full rounded-md group/collapsible ${map[type].border} ${map[type].bg} bg-opacity-10`}
-			defaultOpen={true}
-		>
+		<Collapsible className={`w-full rounded-md group/collapsible ${map[type].border} ${map[type].bg}`}>
 			<CollapsibleTrigger className={"w-full p-1"}>
 				<div className={"flex items-center justify-between w-full gap-2"}>
-					<small className={`font-bold uppercase text-foreground rounded py-1 w-16 ${map[type].bg}`}>{type}</small>
+					<small className={`font-bold uppercase text-foreground rounded py-1 w-16 ${map[type].button}`}>{type}</small>
 					<span className={"font-mono font-bold text-foreground text-left flex-1 truncate"}>{path}</span>
 					<ChevronDown className={"group-data-[state=open]/collapsible:rotate-180"} />
 				</div>
@@ -42,9 +51,9 @@ export default function PlaceholderRequest({ type, path, response, model }: Plac
 			<CollapsibleContent>
 				<div className={"grid gap-3"}>
 					{/* prettier-ignore */}
-					<div className={`flex justify-between w-full bg-background ${map[type].border} border-b-input border-x-0 gap-4 py-2 px-2 sm:px-4`}>
+					<div className={`flex justify-between w-full bg-background ${map[type].border} !border-b-input border-x-0 gap-4 py-2 px-2 sm:px-4`}>
 						<small className={"leading-7 font-bold my-auto"}>Parameters</small>
-						<Link href={path} target={"_blank"}>
+						<Link href={path.replace('{id}', '1')} target={"_blank"}>
 							<Button variant={"outline"} aria-label={"Try it out"}>Try it out</Button>
 						</Link>
 					</div>
@@ -55,19 +64,30 @@ export default function PlaceholderRequest({ type, path, response, model }: Plac
 								Description
 							</small>
 						</div>
-						<div className={"grid grid-cols-12 gap-3 py-2 px-2 sm:px-4"}>
-							<div className={"flex flex-col col-span-3 lg:col-span-2 xl:col-span-1"}>
-								<span className={"font-bold leading-7 text-foreground truncate"}>search</span>
-								<small className={"font-sx font-bold text-foreground truncate"}>string</small>
-								<small className={"font-sx font-bold text-muted-foreground truncate"}>(query)</small>
+						{parameters.map((parameter: PlaceholderRequestParameters, i: number) => (
+							<div className={"grid grid-cols-12 gap-3 py-2 px-2 sm:px-4"} key={i}>
+								<div className={"flex flex-col col-span-3 lg:col-span-2 xl:col-span-1"}>
+									<div className={"font-bold leading-7 text-foreground truncate"}>
+										<span className={"inline-block"}>{parameter.name}</span>
+										{parameter.required && <small className={"text-lg text-destructive px-1"}>*</small>}
+									</div>
+									<small className={"font-sx font-bold text-foreground truncate"}>{parameter.type}</small>
+									<small className={"font-sx font-bold text-muted-foreground truncate"}>({parameter.partOf})</small>
+								</div>
+								<div className={"grid w-full max-w-sm items-center gap-1.5 col-span-9 lg:col-span-10 xl:col-span-11"}>
+									<Label className={"leading-7 font-normal text-foreground"} htmlFor={`${parameter.key}-input`}>
+										{parameter.description}
+									</Label>
+									<Input
+										className={"bg-background"}
+										type={"text"}
+										id={`${parameter.key}-input`}
+										placeholder={parameter.key as string}
+										disabled={true}
+									/>
+								</div>
 							</div>
-							<div className={"grid w-full max-w-sm items-center gap-1.5 col-span-9 lg:col-span-10 xl:col-span-11"}>
-								<Label className={"leading-7 font-normal text-foreground"} htmlFor="input">
-									Search by title and author
-								</Label>
-								<Input className={"bg-background"} type="text" id="input" placeholder="search" />
-							</div>
-						</div>
+						))}
 					</div>
 					<div className={"flex justify-between w-full bg-background border-y border-input gap-4 py-2 px-2 sm:px-4"}>
 						<small className={"leading-7 font-bold my-auto"}>Responses</small>
