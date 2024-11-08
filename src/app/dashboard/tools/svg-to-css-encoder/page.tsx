5e47f28f93540e080a9b23165cc363bb27d2f266
denis23x <Crazy_Code#23>
2024-11-08 18:12:12 +0700

    chore: update home page

--- src/app/dashboard/tools/svg-to-css-encoder/page.tsx
+++ src/app/dashboard/tools/svg-to-css-encoder/page.tsx
@@ -15,11 +15,8 @@ export default function Page() {
 			<h1 className={"scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"}>SVG to CSS Encoder</h1>
 			<p className={"leading-7"}>
 				This tool transforms <strong>SVG</strong> code into a <strong>Data URI format</strong>, creating an encoded{" "}
-				<strong>URL</strong> that can be used directly as a{" "}
-				<code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-					background-image
-				</code>{" "}
-				in <strong>CSS</strong>. Essentially, it lets you embed <strong>SVG</strong> images directly within your
+				<strong>URL</strong> that can be used directly as a <code className="inline-code">background-image</code> in{" "}
+				<strong>CSS</strong>. Essentially, it lets you embed <strong>SVG</strong> images directly within your
 				stylesheets, eliminating the need to host separate image files and reducing server requests for faster loading
 				times.
 			</p>
