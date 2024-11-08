4c181f53a29477ea1cbda0a4ed3e67f8147c1ba6
denis23x <damage.23x@gmail.com>
2024-11-08 19:18:06 +0700

    chore: update home page

--- src/components/app-aurora-background.tsx
+++ src/components/app-aurora-background.tsx
@@ -2,13 +2,10 @@
 
 import { motion } from "framer-motion";
 import { AuroraBackground } from "@/components/ui/aurora-background";
+import { Mouse } from "lucide-react";
 import Link from "next/link";
-// import { FlipWords } from "@/components/ui/flip-words";
-// import { Mouse } from "lucide-react";
 
 export function AppAuroraBackground() {
-	// const words = ["better", "cute", "beautiful", "modern"];
-
 	return (
 		<AuroraBackground>
 			<motion.div
@@ -21,26 +18,19 @@ export function AppAuroraBackground() {
 				}}
 				className="relative flex flex-col gap-4 items-center justify-center px-4"
 			>
-				<div className="text-3xl md:text-7xl font-bold dark:text-white text-center">Hi</div>
-				{/*👋*/}
-				<div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-					And this, is chemical burn.
+				<div className="text-5xl md:text-7xl font-bold dark:text-white text-center">Hi 👋</div>
+				<div className="font-extralight text-2xl md:text-4xl dark:text-neutral-200 md:py-4">
+					And this, is my website.
 				</div>
-				{/*<div className="flex justify-center items-center px-4">*/}
-				{/*	<div className="text-4xl mx-auto font-extralight text-neutral-600 dark:text-neutral-400">*/}
-				{/*		Build*/}
-				{/*		<FlipWords words={words} /> <br />*/}
-				{/*		websites with Aceternity UI*/}
-				{/*	</div>*/}
-				{/*</div>*/}
-				{/*<div className={"mt-10"}>*/}
-				{/*	<Mouse className={"size-12"} />*/}
-				{/*</div>*/}
-				<Link href={"/dashboard"}>
-					<button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+				<div className={"grid gap-10"}>
+					<Link
+						className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2"
+						href={"/dashboard"}
+					>
 						Dashboard
-					</button>
-				</Link>
+					</Link>
+					<Mouse className={"mx-auto text-foreground animate-bounce"} />
+				</div>
 			</motion.div>
 		</AuroraBackground>
 	);
