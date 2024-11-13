2c32b9335e5346a349d24de61c6252441aa9cddd
denis23x <damage.23x@gmail.com>
2024-11-13 14:10:15 +0700

    chore: testing home page lcp

--- src/components/home/home-aurora-background.tsx
+++ src/components/home/home-aurora-background.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import { Mouse } from "lucide-react";
 import Link from "next/link";
@@ -8,16 +8,17 @@ import Link from "next/link";
 export function HomeAuroraBackground() {
 	return (
 		<AuroraBackground>
-			<motion.div
-				initial={{ opacity: 0.0, y: 40 }}
-				whileInView={{ opacity: 1, y: 0 }}
-				transition={{
-					delay: 0.3,
-					duration: 0.8,
-					ease: "easeInOut",
-				}}
-				className="relative flex flex-col gap-4 items-center justify-center px-4"
-			>
+			{/*<motion.div*/}
+			{/*	initial={{ opacity: 0.0, y: 40 }}*/}
+			{/*	whileInView={{ opacity: 1, y: 0 }}*/}
+			{/*	transition={{*/}
+			{/*		delay: 0.3,*/}
+			{/*		duration: 0.8,*/}
+			{/*		ease: "easeInOut",*/}
+			{/*	}}*/}
+			{/*	className="relative flex flex-col gap-4 items-center justify-center px-4"*/}
+			{/*>*/}
+			<div className="relative flex flex-col gap-4 items-center justify-center px-4">
 				<div className="text-5xl md:text-7xl font-bold dark:text-white text-center">Hi 👋</div>
 				<div className="font-extralight text-2xl md:text-4xl dark:text-neutral-200 md:py-4">
 					And this, is My Website.
@@ -31,7 +32,8 @@ export function HomeAuroraBackground() {
 					</Link>
 					<Mouse className={"mx-auto text-foreground animate-bounce"} />
 				</div>
-			</motion.div>
+			</div>
+			{/*</motion.div>*/}
 		</AuroraBackground>
 	);
 }
