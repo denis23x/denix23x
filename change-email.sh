c359c8f9ea6d3f75c755aefdd1a94ad95bfba4a0
denis23x <damage.23x@gmail.com>
2024-11-21 18:07:07 +0700

    fix: test

---
+++ change-email.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+OLD_EMAIL="old-email@example.com"
+CORRECT_NAME="Your Name"
+CORRECT_EMAIL="new-email@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]; then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]; then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
